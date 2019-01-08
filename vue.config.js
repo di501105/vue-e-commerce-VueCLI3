434a1cae30949fa44739ab6ee339d504c111b5cd
DiTseng <paul501105@gmail.com>
2019-01-08 23:13:46 +0800

    vue.config修改

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 module.exports = {
-  baseUrl: '/vue-e-commerce-VueCLI3/',
-  outputDir: 'dist',
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/vue-e-commerce-VueCLI3/'
+    : '/',
 };
